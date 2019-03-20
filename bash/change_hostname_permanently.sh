136d502eb47a25b484af12a06957f15fd88190b0
zeus <a@zagoruiko.org.ua>
2019-03-20 23:59:14 +0200

    docker install script added change hostname script added consul binds to any ip address

---
+++ bash/change_hostname_permanently.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+cp /etc/cloud/cloud.cfg /etc/cloud/cloud.cfg.backup
+cat /etc/cloud/cloud.cfg.backup | sed "/preserve_hostname: false/preserve_hostname: true/g" > /etc/cloud/cloud.cfg
+
+hostnamectl set-hostname $1
--- bash/install_nomad.sh
+++ bash/install_nomad.sh
@@ -44,6 +44,9 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 apt update
 apt install -y docker-ce
+
+cp nomad_files/docker-registry.json /etc/docker/daemon.json
+
 systemctl status docker
 
 
