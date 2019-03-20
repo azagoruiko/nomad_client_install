49c0e6b9969b901b8e16cef9b5680651a13a49c3
zeus <a@zagoruiko.org.ua>
2019-03-21 00:17:44 +0200

    simplify hostanme

--- bash/change_hostname_permanently.sh
+++ bash/change_hostname_permanently.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 cp /etc/cloud/cloud.cfg /etc/cloud/cloud.cfg.backup
-cat /etc/cloud/cloud.cfg.backup | sed "s/preserve_hostname: false/preserve_hostname: true/g" > /etc/cloud/cloud.cfg
+echo "preserve_hostname: true" > /etc/cloud/cloud.cfg
 
 hostnamectl set-hostname $1
